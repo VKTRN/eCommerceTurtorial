17c72036aa24285e8622378e93c9516cbd199a06
Viktor Niedens <viktor.niedens@treubrodt-consulting.de>
2022-03-01 14:21:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
